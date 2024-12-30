7cffa9877fd00a20462aa066cfaea3381cc61744
Hamza Badar <hamzabadar05@gmail.com>
2024-12-30 23:47:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let minsLabel = document.getElementById('minute-label');
 let secsLabel = document.getElementById('second-label');
 
 // Last date of the countdown.
-const lastDate = new Date('07/03/2024');
+const lastDate = new Date('15/11/2025');
 
 // Constant Values Setup
 const second = 1000;
